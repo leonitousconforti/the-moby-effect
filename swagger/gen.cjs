e34cd7f86dc1173e8a18bac7b052db639dd4f363
Leo Conforti <leo@leoconforti.us>
2023-11-27 08:16:02 +0000

    modify swagger codegen templates

--- swagger/gen.cjs
+++ swagger/gen.cjs
@@ -1,12 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const fs = require("node:fs");
 const path = require("node:path");
+const prettier = require("prettier");
 
 const outputDirectory = path.join(__dirname, "../src");
 const templateDirectory = path.join(__dirname, "../src");
 const swaggerFile = path.join(__dirname, "swagger.yaml");
 const swaggerConfig = path.join(__dirname, "swagger-gen.json");
 const swaggerJar = path.join(__dirname, "swagger-codegen-cli.jar");
-const pruneGlob = [`${outputDirectory}/!(api.mustache|api.ts)`, `${outputDirectory}/.*`];
+const pruneGlob = [`${outputDirectory}/!(*.mustache|api.ts)`, `${outputDirectory}/.*`];
 
 module.exports.runAsync = async () => {
     const { $ } = await import("execa");
@@ -14,4 +17,13 @@ module.exports.runAsync = async () => {
 
     await $`java -jar ${swaggerJar} generate --lang typescript-fetch --template-engine mustache --input-spec ${swaggerFile} --config ${swaggerConfig} --output ${outputDirectory} --template-dir ${templateDirectory}`;
     await rimraf(pruneGlob, { glob: true });
+
+    const prettierOptions = {
+        parser: "typescript",
+        ...(await prettier.resolveConfig(process.cwd(), { editorconfig: true })),
+    };
+
+    const source = fs.readFileSync(`${outputDirectory}/api.ts`, "utf8");
+    const formattedSource = await prettier.format(source, prettierOptions);
+    fs.writeFileSync(`${outputDirectory}/api.ts`, formattedSource, "utf8");
 };
