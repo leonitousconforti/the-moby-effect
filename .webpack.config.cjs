03b77d35c2afa77c62613ea74f20fdcd7f42b03c
Leo Conforti <leo@leoconforti.us>
2024-02-02 18:18:08 +0000

    move config files

--- .webpack.config.cjs
+++ .webpack.config.cjs
@@ -5,7 +5,7 @@ module.exports = () => ({
     mode: "production",
     resolve: { extensions: [".js", ".json"] },
     externals: ["bufferutil", "utf-8-validate"],
-    entry: path.join(__dirname, "..", "dist", "bin", "cli.js"),
+    entry: path.join(__dirname, "dist", "bin", "cli.js"),
     module: { rules: [{ test: /\.node$/, loader: "node-loader" }] },
-    output: { path: path.join(__dirname, "..", "dist", "cli"), filename: "cli.cjs" },
+    output: { path: path.join(__dirname, "dist", "cli"), filename: "cli.cjs" },
 });
