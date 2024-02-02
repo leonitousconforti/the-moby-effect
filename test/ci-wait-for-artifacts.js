a4cbb5ac8f6c53667615222aa0b6c6cddb6a64ab
Leo Conforti <leo@leoconforti.us>
2024-02-02 06:41:20 +0000

    save

--- test/ci-wait-for-artifacts.js
+++ test/ci-wait-for-artifacts.js
@@ -1,12 +1,14 @@
 import * as PlatformNode from "@effect/platform-node";
 import * as Octokit from "@octokit/rest";
 import * as Config from "effect/Config";
+import * as Console from "effect/Console";
 import * as Effect from "effect/Effect";
 import * as Function from "effect/Function";
 import * as Schedule from "effect/Schedule";
+import * as NodeFetch from "node-fetch";
 
-const octokit = new Octokit.Octokit();
 const schedule = Schedule.recurs(12).pipe(Schedule.addDelay(() => 5000));
+const octokit = new Octokit.Octokit({ request: { fetch: fetch || NodeFetch.default } });
 
 const hasAllArtifacts = Effect.gen(function* (_) {
     const repo = "the-moby-effect";
@@ -24,6 +26,7 @@ const hasAllArtifacts = Effect.gen(function* (_) {
     );
 
     if (data.total_count !== 2 ** 3) {
+        yield* _(Console.log("Not all artifacts are ready"));
         yield* _(Effect.fail(new Error("Not all artifacts are ready")));
     }
 });
