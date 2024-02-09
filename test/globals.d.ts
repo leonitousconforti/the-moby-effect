9a606336abba7a5989117b32427ef436f0fa70f3
Leo Conforti <leo@leoconforti.us>
2024-02-09 22:08:38 +0000

    update global setup and global teardown scripts for ci

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,3 +1,8 @@
-declare module globalThis {
-    var __THE_MOBY_EFFECT_TEST_URL: string;
+import type { Option } from "effect";
+import type { MobyConnectionOptions } from "../src/index.ts";
+
+declare global {
+    var __DIND_VOLUME_ID: Option.Option<string>;
+    var __DIND_CONTAINER_ID: Option.Option<string>;
+    var __TEST_CONNECTION_OPTIONS: MobyConnectionOptions;
 }
