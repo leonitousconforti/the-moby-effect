1adf612a26842af074e3b545f50626cc3b118ddb
Leo Conforti <leo@leoconforti.us>
2024-02-11 17:26:19 +0000

    use ci-info package

--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,4 @@
+import * as ciInfo from "ci-info";
 import * as Config from "effect/Config";
 import * as Effect from "effect/Effect";
 import * as Option from "effect/Option";
@@ -6,23 +7,6 @@ import * as TestHelpers from "./unit/helpers.js";
 
 export default async function (_globalConfig: unknown, _projectConfig: unknown) {
     await Effect.gen(function* (_: Effect.Adapter) {
-        const node_environment: "production" | "development" | "ci" = yield* _(
-            Config.literal(
-                "production",
-                "development",
-                "ci"
-            )("NODE_ENV").pipe(Config.withDefault("development" as const))
-        );
-
-        /** WTF, how did you manage this?!? */
-        if (node_environment === "production") {
-            return yield* _(
-                Effect.fail(
-                    new Error("What are you doing running this in a production environment? This is a test suite!")
-                )
-            );
-        }
-
         /**
          * If we are anywhere but CI and we aren't running on linux we will
          * refuse to run the test suite because we might not be able to spawn a
@@ -30,7 +14,7 @@ export default async function (_globalConfig: unknown, _projectConfig: unknown) 
          * their docker install) and I really don't like the idea of testing
          * against the users host docker install.
          */
-        if (node_environment !== "ci" && process.platform !== "linux") {
+        if (!ciInfo.isCI && process.platform !== "linux") {
             return yield* _(
                 Effect.fail(
                     new Error(
@@ -53,7 +37,7 @@ export default async function (_globalConfig: unknown, _projectConfig: unknown) 
          * spawn a dind container there because the platform of the runner is
          * not constrained in CI.
          */
-        if (node_environment === "ci" && Option.isNone(testing_host)) {
+        if (ciInfo.isCI && Option.isNone(testing_host)) {
             switch (process.platform) {
                 case "linux":
                 case "darwin": {
@@ -79,7 +63,7 @@ export default async function (_globalConfig: unknown, _projectConfig: unknown) 
          * a dind container there (we already made sure we are on linux above)
          * but we will need to build the dind image first.
          */
-        if (node_environment === "development" && Option.isNone(testing_host)) {
+        if (!ciInfo.isCI && Option.isNone(testing_host)) {
             const intermediate_layer = MobyApi.fromPlatformDefault();
             const [dindContainerId, dindVolumeId, dindConnectionOptions] = yield* _(
                 Effect.provide(
