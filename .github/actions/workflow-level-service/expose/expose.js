486f7ea1093b4e32d58b55337d0b169ca7471dde
Leo Conforti <leo@leoconforti.us>
2024-02-12 22:09:53 +0000

    save

--- .github/actions/workflow-level-service/expose/expose.js
+++ .github/actions/workflow-level-service/expose/expose.js
@@ -78,6 +78,7 @@ const processConnectionRequest = Effect.gen(function* (_) {
         const { privateKey, publicKey } = yield* _(Effect.promise(() => wireguard.generateKeyPair()));
 
         const hostConfig = new wireguard.WgConfig({
+            filePath: "/etc/wireguard/wg0.conf",
             wgInterface: {
                 name: "wg0",
                 privateKey,
