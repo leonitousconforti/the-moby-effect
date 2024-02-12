72a912fe5bd253f0bb5d2ff985c0d0c6a982b627
Leo Conforti <leo@leoconforti.us>
2024-02-12 21:48:43 +0000

    save

--- .github/actions/workflow-level-service/connect/connect.js
+++ .github/actions/workflow-level-service/connect/connect.js
@@ -26,6 +26,7 @@ const uploadConnectionRequestArtifact = Effect.gen(function* (_) {
     const stunResponse = yield* _(
         Effect.promise(() => stun.request("stun.l.google.com:19302", { socket: stunSocket }))
     );
+    console.log(stunResponse);
     const mappedAddress = stunResponse.getAttribute(stun.constants.STUN_ATTR_MAPPED_ADDRESS).value;
     const myLocation = `${mappedAddress.address}:${mappedAddress.port}:${stunSocket.address().port}`;
     yield* _(
