915a97ca7037b77d8c42133d92269a127b9fbcc5
Leo Conforti <leo@leoconforti.us>
2024-07-13 03:30:07 +0000

    rename type vars

--- src/demux/Raw.ts
+++ src/demux/Raw.ts
@@ -102,17 +102,17 @@ export const demuxRawSocket = Function.dual<
  * @see https://docs.docker.com/engine/api/v1.46/#tag/Container/operation/ContainerAttachWebsocket
  */
 export const demuxRawSockets = <
-    S1 extends readonly [Stream.Stream<string | Uint8Array, unknown, unknown>, RawStreamSocket],
-    S2 extends readonly [RawStreamSocket, Sink.Sink<unknown, string, string, unknown, unknown>],
-    S3 extends readonly [RawStreamSocket, Sink.Sink<unknown, string, string, unknown, unknown>],
-    A1 = S2 extends [RawStreamSocket, Sink.Sink<infer A, string, string, infer _E, infer _R>] ? A : undefined,
-    A2 = S3 extends [RawStreamSocket, Sink.Sink<infer A, string, string, infer _E, infer _R>] ? A : undefined,
-    E1 = S1 extends [Stream.Stream<string | Uint8Array, infer E, infer _R>, RawStreamSocket] ? E : never,
-    E2 = S2 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer E, infer _R>] ? E : never,
-    E3 = S3 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer E, infer _R>] ? E : never,
-    R1 = S1 extends [Stream.Stream<string | Uint8Array, infer _E, infer R>, RawStreamSocket] ? R : never,
-    R2 = S2 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer _E, infer R>] ? R : never,
-    R3 = S3 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer _E, infer R>] ? R : never,
+    O1 extends readonly [Stream.Stream<string | Uint8Array, unknown, unknown>, RawStreamSocket],
+    O2 extends readonly [RawStreamSocket, Sink.Sink<unknown, string, string, unknown, unknown>],
+    O3 extends readonly [RawStreamSocket, Sink.Sink<unknown, string, string, unknown, unknown>],
+    A1 = O2 extends [RawStreamSocket, Sink.Sink<infer A, string, string, infer _E, infer _R>] ? A : undefined,
+    A2 = O3 extends [RawStreamSocket, Sink.Sink<infer A, string, string, infer _E, infer _R>] ? A : undefined,
+    E1 = O1 extends [Stream.Stream<string | Uint8Array, infer E, infer _R>, RawStreamSocket] ? E : never,
+    E2 = O2 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer E, infer _R>] ? E : never,
+    E3 = O3 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer E, infer _R>] ? E : never,
+    R1 = O1 extends [Stream.Stream<string | Uint8Array, infer _E, infer R>, RawStreamSocket] ? R : never,
+    R2 = O2 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer _E, infer R>] ? R : never,
+    R3 = O3 extends [RawStreamSocket, Sink.Sink<infer _A, string, string, infer _E, infer R>] ? R : never,
     A = A1 extends undefined | void
         ? A2 extends undefined | void
             ? void
@@ -122,21 +122,21 @@ export const demuxRawSockets = <
           : readonly [stdout: A1, stderr: A2],
 >(
     streams:
-        | { stdin: S1; stdout?: never; stderr?: never }
-        | { stdin?: never; stdout: S2; stderr?: never }
-        | { stdin?: never; stdout?: never; stderr: S3 }
-        | { stdin: S1; stdout: S2; stderr?: never }
-        | { stdin: S1; stdout?: never; stderr: S3 }
-        | { stdin?: never; stdout: S2; stderr: S3 }
-        | { stdin: S1; stdout: S2; stderr: S3 }
+        | { stdin: O1; stdout?: never; stderr?: never }
+        | { stdin?: never; stdout: O2; stderr?: never }
+        | { stdin?: never; stdout?: never; stderr: O3 }
+        | { stdin: O1; stdout: O2; stderr?: never }
+        | { stdin: O1; stdout?: never; stderr: O3 }
+        | { stdin?: never; stdout: O2; stderr: O3 }
+        | { stdin: O1; stdout: O2; stderr: O3 }
 ): Effect.Effect<
     A,
     E1 | E2 | E3 | Socket.SocketError,
     Exclude<R1, Scope.Scope> | Exclude<R2, Scope.Scope> | Exclude<R3, Scope.Scope>
 > => {
-    type S4 = Stream.Stream<string | Uint8Array, E1, R1>;
-    type S5 = Sink.Sink<A1, string, string, E2, R2>;
-    type S6 = Sink.Sink<A2, string, string, E3, R3>;
+    type S1 = Stream.Stream<string | Uint8Array, E1, R1>;
+    type S2 = Sink.Sink<A1, string, string, E2, R2>;
+    type S3 = Sink.Sink<A2, string, string, E3, R3>;
     type StdinEffect = Effect.Effect<void, E1 | Socket.SocketError, Exclude<R1, Scope.Scope>>;
     type StdoutEffect = Effect.Effect<A1, E2 | Socket.SocketError, Exclude<R2, Scope.Scope>>;
     type StderrEffect = Effect.Effect<A2, E3 | Socket.SocketError, Exclude<R3, Scope.Scope>>;
@@ -144,15 +144,15 @@ export const demuxRawSockets = <
     const { stderr, stdin, stdout } = streams;
 
     const runStdin: StdinEffect = Predicate.isNotUndefined(stdin)
-        ? demuxRawSocket(stdin[1], stdin[0] as S4, Sink.drain)
+        ? demuxRawSocket(stdin[1], stdin[0] as S1, Sink.drain)
         : Effect.void;
 
     const runStdout: StdoutEffect = Predicate.isNotUndefined(stdout)
-        ? demuxRawSocket(stdout[0], Stream.never, stdout[1] as S5)
+        ? demuxRawSocket(stdout[0], Stream.never, stdout[1] as S2)
         : Function.unsafeCoerce(Effect.sync(Function.constUndefined));
 
     const runStderr: StderrEffect = stderr
-        ? demuxRawSocket(stderr[0], Stream.never, stderr[1] as S6)
+        ? demuxRawSocket(stderr[0], Stream.never, stderr[1] as S3)
         : Function.unsafeCoerce(Effect.sync(Function.constUndefined));
 
     return Effect.map(
