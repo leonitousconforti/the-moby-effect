e906d462dd551b3214b3ce1c90964cc6d349dd9d
Leo Conforti <leo@leoconforti.us>
2024-08-31 07:07:52 +0000

    test

--- src/convey/Streams.ts
+++ src/convey/Streams.ts
@@ -6,7 +6,6 @@
 
 import * as Effect from "effect/Effect";
 import * as Function from "effect/Function";
-import * as Option from "effect/Option";
 import * as Stream from "effect/Stream";
 
 import * as Images from "../endpoints/Images.js";
@@ -23,7 +22,9 @@ import * as Images from "../endpoints/Images.js";
 export const packBuildContextIntoTarballStreamServer = (
     cwd: string,
     entries: Array<string> = ["dockerfile"]
-): Stream.Stream<Uint8Array, Images.ImagesError, never> =>
+): [unknown] extends [typeof import("tar-fs")]
+    ? "Missing tar-fs package"
+    : Stream.Stream<Uint8Array, Images.ImagesError, never> =>
     Function.pipe(
         Effect.promise(() => import("tar-fs")),
         Effect.map((tar) =>
@@ -46,25 +47,3 @@ export const packBuildContextIntoTarballStreamServer = (
 export const packBuildContextIntoTarballStreamWeb = (
     entries: Record<string, string | Uint8Array>
 ): Stream.Stream<Uint8Array, Images.ImagesError, never> => Stream.never as any;
-
-Stream.unfold(new Uint8Array(), (buffer) => Option.none());
-
-// struct posix_header {          // byte offset
-// 	char name[100];               // 0
-// 	char mode[8];                 // 100
-// 	char uid[8];                  // 108
-// 	char gid[8];                  // 116
-// 	char size[12];                // 124
-// 	char mtime[12];               // 136
-// 	char chksum[8];               // 148
-// 	char typeflag;                // 156
-// 	char linkname[100];           // 157
-// 	char magic[6];                // 257
-// 	char version[2];              // 263
-// 	char uname[32];               // 265
-// 	char gname[32];               // 297
-// 	char devmajor[8];             // 329
-// 	char devminor[8];             // 337
-// 	char prefix[155];             // 345
-//                                // 500
-// };
