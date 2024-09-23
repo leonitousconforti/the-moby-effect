5f06d97bbeac47715b7d24b094608727217e5381
Leo Conforti <leo@leoconforti.us>
2024-09-23 20:07:40 +0000

    update playground

--- scratchpad/playground.ts
+++ scratchpad/playground.ts
@@ -2,18 +2,20 @@ import * as NodeContext from "@effect/platform-node/NodeContext";
 import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
 import * as Console from "effect/Console";
 import * as Effect from "effect/Effect";
+import * as Layer from "effect/Layer";
 
 import * as Platforms from "the-moby-effect/Platforms";
 import * as System from "the-moby-effect/endpoints/System";
 import * as DindEngine from "the-moby-effect/engines/Dind";
 
-const connectionOptions = Effect.runSync(Platforms.connectionOptionsFromPlatformSystemSocketDefault());
-
-const dindLayer = DindEngine.layerNodeJS({
-    exposeDindContainerBy: "socket" as const,
-    connectionOptionsToHost: connectionOptions,
-    dindBaseImage: "docker.io/library/docker:23-dind-rootless" as const,
-});
+const dindLayer = Effect.gen(function* () {
+    const connectionOptions = yield* Platforms.connectionOptionsFromPlatformSystemSocketDefault();
+    return DindEngine.layerNodeJS({
+        exposeDindContainerBy: "socket" as const,
+        connectionOptionsToHost: connectionOptions,
+        dindBaseImage: "docker.io/library/docker:23-dind-rootless" as const,
+    });
+}).pipe(Layer.unwrapEffect);
 
 Effect.gen(function* () {
     const result = yield* System.Systems.info();
