6876aea117213f1f9f4dd9570f9594f7d265ce69
Leo Conforti <leoconforti@ltgk.net>
2024-10-17 20:16:33 +0000

    fix sink impl

--- src/convey/Sinks.ts
+++ src/convey/Sinks.ts
@@ -7,11 +7,11 @@
 import * as Chunk from "effect/Chunk";
 import * as Console from "effect/Console";
 import * as Effect from "effect/Effect";
+import * as Function from "effect/Function";
 import * as Predicate from "effect/Predicate";
 import * as Scope from "effect/Scope";
 import * as Sink from "effect/Sink";
 import * as Stream from "effect/Stream";
-import * as Tuple from "effect/Tuple";
 
 import { JSONMessage } from "../generated/JSONMessage.generated.js";
 
@@ -52,14 +52,4 @@ export const followProgressSink = Sink.forEach<JSONMessage, void, never, never>(
 export const followProgressInConsole = <E1, R1>(
     stream: Stream.Stream<JSONMessage, E1, R1>
 ): Effect.Effect<Chunk.Chunk<JSONMessage>, E1, Exclude<R1, Scope.Scope>> =>
-    Effect.gen(function* () {
-        const firstStream = stream;
-        const secondStream = yield* Stream.broadcastDynamic(stream, 16);
-        const effects = Tuple.make(
-            waitForProgressToComplete(firstStream),
-            Stream.run(secondStream, followProgressSink)
-        );
-        return yield* Effect.all(effects, { concurrency: 2 });
-    })
-        .pipe(Effect.scoped)
-        .pipe(Effect.map(Tuple.getFirst));
+    Function.pipe(stream, Stream.tapSink(followProgressSink), waitForProgressToComplete);
