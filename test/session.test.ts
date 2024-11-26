e5cd2d860e7e18f1bb9e2d5867977c37389aee20
Leo Conforti <leoconforti@ltgk.net>
2024-11-26 23:05:14 +0000

    update skip if for session tests

--- test/session.test.ts
+++ test/session.test.ts
@@ -4,10 +4,12 @@ import { Sessions } from "the-moby-effect/MobyEndpoints";
 import { testLayer } from "./shared.js";
 
 layer(Layer.fresh(testLayer), { timeout: Duration.minutes(2) })("MobyApi Session tests", (it) => {
-    it.scoped.skipIf(inject("__PLATFORM_VARIANT").includes("undici"))("Should be able to request a session", () =>
-        Effect.gen(function* () {
-            const sessions = yield* Sessions;
-            yield* sessions.session();
-        })
+    it.scoped.skipIf(inject("__PLATFORM_VARIANT").includes("undici") || inject("__PLATFORM_VARIANT").includes("deno"))(
+        "Should be able to request a session",
+        () =>
+            Effect.gen(function* () {
+                const sessions = yield* Sessions;
+                yield* sessions.session();
+            })
     );
 });
