c4e2d01ce4fc7e992e8516d2c994e7a4e5329916
Leo Conforti <leoconforti@ltgk.net>
2024-11-26 23:00:56 +0000

    fix needs

--- src/platforms/Needs.ts
+++ src/platforms/Needs.ts
@@ -4,8 +4,8 @@
  * @since 1.0.0
  */
 
-// import type * as Effect from "effect/Effect";
-// import type * as Layer from "effect/Layer";
+import type * as Effect from "effect/Effect";
+import type * as Layer from "effect/Layer";
 
 /**
  * @since 1.0.0
@@ -18,64 +18,63 @@ export type HasPackage<T> = [unknown] extends [T] ? "no" : "yes";
  * @since 1.0.0
  * @category Types
  */
-// export type NeedsSSH2<Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>> =
-//     HasPackage<typeof import("ssh2")> extends "yes"
-//         ? Dependent
-//         : Dependent extends Effect.Effect<unknown, unknown, unknown>
-//           ? Effect.Effect<never, 'Missing "ssh2" package', never>
-//           : Layer.Layer<unknown, 'Missing "ssh2" package', never>;
+export type NeedsSSH2<
+    Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>,
+> = HasPackage<typeof import("ssh2")> extends "yes" ? Dependent : 'Missing "ssh2" package';
 
 /**
  * @since 1.0.0
  * @category Types
  */
-// export type NeedsUndici<Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>> =
-//     HasPackage<typeof import("undici")> extends "yes"
-//         ? Dependent
-//         : Dependent extends Effect.Effect<unknown, unknown, unknown>
-//           ? Effect.Effect<never, 'Missing "undici" package', never>
-//           : Layer.Layer<unknown, 'Missing "undici" package', never>;
+export type NeedsUndici<
+    Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>,
+> = HasPackage<typeof import("undici")> extends "yes" ? Dependent : 'Missing "undici" package';
 
 /**
  * @since 1.0.0
  * @category Types
  */
-// export type NeedsNodeHttp<Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>> =
-//     HasPackage<typeof import("node:http")> extends "yes"
-//         ? Dependent
-//         : Dependent extends Effect.Effect<unknown, unknown, unknown>
-//           ? Effect.Effect<never, 'Missing "node:http" package', never>
-//           : Layer.Layer<unknown, 'Missing "node:http" package', never>;
+export type NeedsNodeHttp<
+    Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>,
+> = HasPackage<typeof import("node:http")> extends "yes" ? Dependent : 'Missing "node:http" package';
 
 /**
  * @since 1.0.0
  * @category Types
  */
-// export type NeedsNodeHttps<Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>> =
-//     HasPackage<typeof import("node:https")> extends "yes"
-//         ? Dependent
-//         : Dependent extends Effect.Effect<unknown, unknown, unknown>
-//           ? Effect.Effect<never, 'Missing "node:https" package', never>
-//           : Layer.Layer<unknown, 'Missing "node:https" package', never>;
+export type NeedsNodeHttps<
+    Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>,
+> = HasPackage<typeof import("node:https")> extends "yes" ? Dependent : 'Missing "node:https" package';
 
 /**
  * @since 1.0.0
  * @category Types
  */
-// export type NeedsPlatformNode<Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>> =
-//     HasPackage<typeof import("@effect/platform-node")> extends "yes"
-//         ? Dependent
-//         : Dependent extends Effect.Effect<unknown, unknown, unknown>
-//           ? Effect.Effect<never, 'Missing "@effect/platform-node" package', never>
-//           : Layer.Layer<unknown, 'Missing "@effect/platform-node" package', never>;
+export type NeedsPlatformNode<
+    Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>,
+> =
+    HasPackage<typeof import("@effect/platform-node")> extends "yes"
+        ? Dependent
+        : 'Missing "@effect/platform-node" package';
 
 /**
  * @since 1.0.0
  * @category Types
  */
-// export type NeedsPlatformBrowser<Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>> =
-//     HasPackage<typeof import("@effect/platform-browser")> extends "yes"
-//         ? Dependent
-//         : Dependent extends Effect.Effect<unknown, unknown, unknown>
-//           ? Effect.Effect<never, 'Missing "@effect/platform-browser" package', never>
-//           : Layer.Layer<unknown, 'Missing "@effect/platform-browser" package', never>;
+export type NeedsPlatformBun<
+    Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>,
+> =
+    HasPackage<typeof import("@effect/platform-bun")> extends "yes"
+        ? Dependent
+        : 'Missing "@effect/platform-bun" package';
+
+/**
+ * @since 1.0.0
+ * @category Types
+ */
+export type NeedsPlatformBrowser<
+    Dependent extends Effect.Effect<unknown, unknown, unknown> | Layer.Layer<unknown, unknown, unknown>,
+> =
+    HasPackage<typeof import("@effect/platform-browser")> extends "yes"
+        ? Dependent
+        : 'Missing "@effect/platform-browser" package';
