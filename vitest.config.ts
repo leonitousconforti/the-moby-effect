68315509925a4cb7f1988eecaf168c33b72f4c0a
Leo Conforti <leoconforti@ltgk.net>
2025-03-11 16:34:08 +0000

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,28 @@ export default defineConfig({
         setupFiles: ["./test/setup-file.ts"],
         globalSetup: "./test/setup-global.ts",
         include: ["./test/**/*.test.ts"],
-        coverage: { provider: "v8", include: ["src/**/*.ts"], reporter: ["cobertura", "text"] },
+        coverage: {
+            provider: "v8",
+            include: ["src/**/*.ts"],
+            exclude: ["src/generated/**/*.ts", "src/schemas/**/*.ts", "src/blob/**/*.ts"],
+            reporter: ["cobertura", "text"],
+        },
         reporters: ["default", "hanging-process", ["junit", { outputFile: "./coverage/junit.xml" }]],
+    },
+    server: {
+        watch: {
+            ignored: [
+                "**/node_modules/**",
+                "**/.git/**",
+                "**/submodules/**",
+                "**/patches/**",
+                "**/dist/**",
+                "**/build/**",
+                "**/experiments/**",
+                "**/coverage/**",
+                "**/ui/**",
+            ],
+        },
     },
     resolve: {
         alias: {
