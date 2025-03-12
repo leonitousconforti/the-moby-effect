498c2c837a389389ef258a92d5fbea05fad05405
Leo Conforti <leoconforti@ltgk.net>
2025-03-12 15:02:18 +0000

    simplify interleaving

--- src/demux/Interleave.ts
+++ src/demux/Interleave.ts
@@ -5,62 +5,39 @@
  */
 
 import * as Socket from "@effect/platform/Socket";
-import * as Effect from "effect/Effect";
 import * as Stream from "effect/Stream";
 
-import { isMultiplexedStreamSocket, makeMultiplexedStreamSocket, MultiplexedStreamSocket } from "./Multiplexed.js";
-import { makeRawStreamSocket, RawStreamSocket } from "./Raw.js";
+import { rawSocketToStream, RawStreamSocket } from "./Raw.js";
 
 /**
- * Interleave stdout and stderr streams back into a socket.
+ * Interleaves an stdout socket with an stderr socket.
  *
  * @since 1.0.0
  * @category Interleave
  */
-export const interleaveBackToSocket: {
-    // Interleave raw streams.
-    (stdout: RawStreamSocket, stderr: RawStreamSocket): Effect.Effect<RawStreamSocket, never, never>;
-    // Interleave multiplexed streams.
-    (
-        stdout: MultiplexedStreamSocket,
-        stderr: MultiplexedStreamSocket
-    ): Effect.Effect<MultiplexedStreamSocket, never, never>;
-} = <T extends MultiplexedStreamSocket | RawStreamSocket>(stdout: T, stderr: T): Effect.Effect<T, never, never> =>
-    Effect.gen(function* () {
-        const stdoutChannel = Socket.toChannel(stdout);
-        const stderrChannel = Socket.toChannel(stderr);
-        const stdoutStream = Stream.pipeThroughChannelOrFail(Stream.never, stdoutChannel);
-        const stderrStream = Stream.pipeThroughChannelOrFail(Stream.never, stderrChannel);
-        const stream = Stream.interleave(stdoutStream, stderrStream);
-        const readable = Stream.toReadableStream(stream);
-        const transformStream = Effect.succeed({ readable, writable: new WritableStream() });
-        const socket = yield* Socket.fromTransformStream(transformStream);
-        return isMultiplexedStreamSocket(stdout)
-            ? (makeMultiplexedStreamSocket(socket) as T)
-            : (makeRawStreamSocket(socket) as T);
-    });
+export const interleaveToStream = (
+    stdout: RawStreamSocket,
+    stderr: RawStreamSocket
+): Stream.Stream<Uint8Array, Socket.SocketError, never> =>
+    Stream.interleave(rawSocketToStream(stdout), rawSocketToStream(stderr));
 
 /**
- * Interleave stdout and stderr streams.
+ * Interleaves an stdout socket with an stderr socket and tags the output
+ * stream.
  *
  * @since 1.0.0
  * @category Interleave
  */
-export const interleaveToStream: {
-    // Interleave raw streams.
-    (stdout: RawStreamSocket, stderr: RawStreamSocket): Stream.Stream<Uint8Array, Socket.SocketError, never>;
-    // Interleave multiplexed streams.
-    (
-        stdout: MultiplexedStreamSocket,
-        stderr: MultiplexedStreamSocket
-    ): Stream.Stream<Uint8Array, Socket.SocketError, never>;
-} = <T extends MultiplexedStreamSocket | RawStreamSocket>(
-    stdout: T,
-    stderr: T
-): Stream.Stream<Uint8Array, Socket.SocketError, never> => {
-    const stdoutChannel = Socket.toChannel<never>(stdout);
-    const stderrChannel = Socket.toChannel<never>(stderr);
-    const stdoutStream = Stream.pipeThroughChannelOrFail(Stream.empty, stdoutChannel);
-    const stderrStream = Stream.pipeThroughChannelOrFail(Stream.empty, stderrChannel);
-    return Stream.interleave(stdoutStream, stderrStream);
-};
+export const interleaveToTaggedStream = (
+    stdout: RawStreamSocket,
+    stderr: RawStreamSocket,
+    options?: { bufferSize?: number | undefined } | undefined
+): Stream.Stream<
+    { _tag: "stdout"; value: Uint8Array } | { _tag: "stderr"; value: Uint8Array },
+    Socket.SocketError,
+    never
+> =>
+    Stream.mergeWithTag(
+        { stdout: rawSocketToStream(stdout), stderr: rawSocketToStream(stderr) } as const,
+        { concurrency: "unbounded", ...options } as const
+    );
