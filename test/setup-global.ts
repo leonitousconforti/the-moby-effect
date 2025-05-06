ac1e719c98c813ab93dd809f0c01ee239cf7a97d
Leo Conforti <leoconforti@ltgk.net>
2025-05-06 15:30:05 +0000

    pull all base images before starting test suite

--- test/setup-global.ts
+++ test/setup-global.ts
@@ -1,7 +1,19 @@
 import type { TestProject } from "vitest/node";
 
+import * as Array from "effect/Array";
 import * as Config from "effect/Config";
 import * as Effect from "effect/Effect";
+import * as Function from "effect/Function";
+import * as Layer from "effect/Layer";
+
+import { DockerEngine, MobyConnection, MobyConvey } from "the-moby-effect";
+import { testMatrix } from "./shared.js";
+
+const localDocker = Function.pipe(
+    MobyConnection.connectionOptionsFromPlatformSystemSocketDefault,
+    Effect.map(DockerEngine.layerNodeJS),
+    Layer.unwrapEffect
+);
 
 export const setup = async function ({ provide }: TestProject): Promise<void> {
     await Effect.gen(function* () {
@@ -18,5 +30,18 @@ export const setup = async function ({ provide }: TestProject): Promise<void> {
         )("__PLATFORM_VARIANT").pipe(Config.orElse(() => Config.succeed("node-18.x" as const)));
 
         provide("__PLATFORM_VARIANT", platformVariant);
-    }).pipe(Effect.runPromise);
+
+        const images = Function.pipe(
+            testMatrix,
+            Array.map(({ dindBaseImage }) => dindBaseImage),
+            Array.dedupe
+        );
+
+        for (const image of images) {
+            const pullStream = DockerEngine.pull({ image });
+            yield* MobyConvey.waitForProgressToComplete(pullStream);
+        }
+    })
+        .pipe(Effect.provide(localDocker))
+        .pipe(Effect.runPromise);
 };
