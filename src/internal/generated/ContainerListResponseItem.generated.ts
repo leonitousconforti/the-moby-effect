5d5adaaa46a17acf43901eac00688c09328cb4a6
Leo Conforti <leoconforti@ltgk.net>
2025-05-07 17:27:02 +0000

    save

--- src/internal/generated/ContainerListResponseItem.generated.ts
+++ src/internal/generated/ContainerListResponseItem.generated.ts
@@ -1,8 +1,9 @@
 import * as Schema from "effect/Schema";
 import * as MobySchemas from "../schemas/index.js";
+import * as Descriptor from "./Descriptor.generated.js";
 import * as MountPoint from "./MountPoint.generated.js";
+import * as NetworkSettingsSummary from "./NetworkSettingsSummary.generated.js";
 import * as Port from "./Port.generated.js";
-import * as SummaryNetworkSettings from "./SummaryNetworkSettings.generated.js";
 
 export class ContainerListResponseItem extends Schema.Class<ContainerListResponseItem>("ContainerListResponseItem")(
     {
@@ -10,36 +11,28 @@ export class ContainerListResponseItem extends Schema.Class<ContainerListRespons
         Names: Schema.NullOr(Schema.Array(Schema.String)),
         Image: Schema.String,
         ImageID: Schema.String,
+        ImageManifestDescriptor: Schema.optionalWith(Descriptor.Descriptor, { nullable: true }),
         Command: Schema.String,
         Created: MobySchemas.Int64,
         Ports: Schema.NullOr(Schema.Array(Schema.NullOr(Port.Port))),
         SizeRw: Schema.optional(MobySchemas.Int64),
         SizeRootFs: Schema.optional(MobySchemas.Int64),
-        Labels: Schema.NullOr(
-            Schema.Record({
-                key: Schema.String,
-                value: Schema.String,
-            })
-        ),
+        Labels: Schema.NullOr(Schema.Record({ key: Schema.String, value: Schema.String })),
         State: Schema.String,
         Status: Schema.String,
         HostConfig: Schema.Struct({
             NetworkMode: Schema.optional(Schema.String),
-            Annotations: Schema.optionalWith(
-                Schema.Record({
-                    key: Schema.String,
-                    value: Schema.String,
-                }),
-                { nullable: true }
-            ),
+            Annotations: Schema.optionalWith(Schema.Record({ key: Schema.String, value: Schema.String }), {
+                nullable: true,
+            }),
         }),
-        NetworkSettings: Schema.NullOr(SummaryNetworkSettings.SummaryNetworkSettings),
+        NetworkSettings: Schema.NullOr(NetworkSettingsSummary.NetworkSettingsSummary),
         Mounts: Schema.NullOr(Schema.Array(Schema.NullOr(MountPoint.MountPoint))),
     },
     {
         identifier: "ContainerListResponseItem",
-        title: "types.Container",
+        title: "container.Summary",
         documentation:
-            "https://github.com/moby/moby/blob/a21b1a2d12e2c01542cb191eb526d7bfad0641e3/api/types/types.go#L140-L161",
+            "https://github.com/moby/moby/blob/453c165be709d294ab744f2efbd2552b338bb1a0/api/types/container/container.go#L121-L143",
     }
 ) {}
