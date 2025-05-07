1ee123fe3654e42bf4c5c0a9d1f3a7210150da55
Leo Conforti <leoconforti@ltgk.net>
2025-05-07 03:23:10 +0000

    save

--- src/internal/generated/ContainerResources.generated.ts
+++ src/internal/generated/ContainerResources.generated.ts
@@ -105,6 +105,6 @@ export class ContainerResources extends Schema.Class<ContainerResources>("Contai
         identifier: "ContainerResources",
         title: "container.Resources",
         documentation:
-            "https://github.com/moby/moby/blob/a21b1a2d12e2c01542cb191eb526d7bfad0641e3/api/types/container/hostconfig.go#L369-L410",
+            "https://github.com/moby/moby/blob/453c165be709d294ab744f2efbd2552b338bb1a0/api/types/container/hostconfig.go#L370-L411",
     }
 ) {}
--- src/internal/generated/ContainerRestartPolicy.generated.ts
+++ src/internal/generated/ContainerRestartPolicy.generated.ts
@@ -3,13 +3,16 @@ import * as MobySchemas from "../schemas/index.js";
 
 export class ContainerRestartPolicy extends Schema.Class<ContainerRestartPolicy>("ContainerRestartPolicy")(
     {
-        Name: Schema.Literal("no", "always", "unless-stopped", "on-failure"),
+        Name: Schema.Literal("no", "always", "unless-stopped", "on-failure").annotations({
+            documentation:
+                "https://github.com/moby/moby/blob/453c165be709d294ab744f2efbd2552b338bb1a0/api/types/container/hostconfig.go#L281-L288",
+        }),
         MaximumRetryCount: MobySchemas.Int64,
     },
     {
         identifier: "ContainerRestartPolicy",
         title: "container.RestartPolicy",
         documentation:
-            "https://github.com/moby/moby/blob/a21b1a2d12e2c01542cb191eb526d7bfad0641e3/api/types/container/hostconfig.go#L274-L278",
+            "https://github.com/moby/moby/blob/453c165be709d294ab744f2efbd2552b338bb1a0/api/types/container/hostconfig.go#L275-L279",
     }
 ) {}
