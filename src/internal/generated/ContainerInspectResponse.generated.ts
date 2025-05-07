fe8aec1099d19098eb96a9beaa0966fad1c0710c
Leo Conforti <leoconforti@ltgk.net>
2025-05-07 17:25:12 +0000

    save

--- src/internal/generated/ContainerInspectResponse.generated.ts
+++ src/internal/generated/ContainerInspectResponse.generated.ts
@@ -1,44 +1,22 @@
 import * as Schema from "effect/Schema";
-import * as MobySchemas from "../schemas/index.js";
 import * as ContainerConfig from "./ContainerConfig.generated.js";
-import * as ContainerHostConfig from "./ContainerHostConfig.generated.js";
-import * as ContainerNode from "./ContainerNode.generated.js";
-import * as ContainerState from "./ContainerState.generated.js";
-import * as GraphDriverData from "./GraphDriverData.generated.js";
+import * as ContainerJSONBase from "./ContainerJSONBase.generated.js";
+import * as Descriptor from "./Descriptor.generated.js";
 import * as MountPoint from "./MountPoint.generated.js";
 import * as NetworkSettings from "./NetworkSettings.generated.js";
 
 export class ContainerInspectResponse extends Schema.Class<ContainerInspectResponse>("ContainerInspectResponse")(
     {
-        Id: Schema.String,
-        Created: Schema.String,
-        Path: Schema.String,
-        Args: Schema.NullOr(Schema.Array(Schema.String)),
-        State: Schema.NullOr(ContainerState.ContainerState),
-        Image: Schema.String,
-        ResolvConfPath: Schema.String,
-        HostnamePath: Schema.String,
-        HostsPath: Schema.String,
-        LogPath: Schema.String,
-        Node: Schema.optionalWith(ContainerNode.ContainerNode, { nullable: true }),
-        Name: Schema.String,
-        RestartCount: MobySchemas.Int64,
-        Driver: Schema.String,
-        Platform: Schema.String,
-        MountLabel: Schema.String,
-        ProcessLabel: Schema.String,
-        AppArmorProfile: Schema.String,
-        ExecIDs: Schema.NullOr(Schema.Array(Schema.String)),
-        HostConfig: Schema.NullOr(ContainerHostConfig.ContainerHostConfig),
-        GraphDriver: Schema.NullOr(GraphDriverData.GraphDriverData),
-        SizeRw: Schema.optionalWith(MobySchemas.Int64, { nullable: true }),
-        SizeRootFs: Schema.optionalWith(MobySchemas.Int64, { nullable: true }),
+        ...ContainerJSONBase.ContainerJSONBase.fields,
         Mounts: Schema.NullOr(Schema.Array(Schema.NullOr(MountPoint.MountPoint))),
         Config: Schema.NullOr(ContainerConfig.ContainerConfig),
         NetworkSettings: Schema.NullOr(NetworkSettings.NetworkSettings),
+        ImageManifestDescriptor: Schema.optionalWith(Descriptor.Descriptor, { nullable: true }),
     },
     {
         identifier: "ContainerInspectResponse",
-        title: "types.ContainerJSON",
+        title: "container.InspectResponse",
+        documentation:
+            "https://github.com/moby/moby/blob/453c165be709d294ab744f2efbd2552b338bb1a0/api/types/container/container.go#L179-L188",
     }
 ) {}
