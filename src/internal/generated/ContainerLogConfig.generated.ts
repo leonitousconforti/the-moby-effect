d2f6c35855216b0ec3f55dfbd094c791d4df4977
Leo Conforti <leoconforti@ltgk.net>
2025-05-07 03:17:47 +0000

    save

--- src/internal/generated/ContainerLogConfig.generated.ts
+++ src/internal/generated/ContainerLogConfig.generated.ts
@@ -3,17 +3,12 @@ import * as Schema from "effect/Schema";
 export class ContainerLogConfig extends Schema.Class<ContainerLogConfig>("ContainerLogConfig")(
     {
         Type: Schema.String,
-        Config: Schema.NullOr(
-            Schema.Record({
-                key: Schema.String,
-                value: Schema.String,
-            })
-        ),
+        Config: Schema.NullOr(Schema.Record({ key: Schema.String, value: Schema.String })),
     },
     {
         identifier: "ContainerLogConfig",
         title: "container.LogConfig",
         documentation:
-            "https://github.com/moby/moby/blob/a21b1a2d12e2c01542cb191eb526d7bfad0641e3/api/types/container/hostconfig.go#L357-L361",
+            "https://github.com/moby/moby/blob/453c165be709d294ab744f2efbd2552b338bb1a0/api/types/container/hostconfig.go#L358-L362",
     }
 ) {}
