9831c27449ea42fce7ce17627e509bb4f9bdd675
Leo Conforti <leo@leoconforti.us>
2025-11-12 10:47:12 -0600

    revert

--- test/containers.test.ts
+++ test/containers.test.ts
@@ -28,12 +28,12 @@ describe.each(testMatrix)(
                 () =>
                     Effect.gen(function* () {
                         const containers = yield* MobyEndpoints.Containers;
-                        const pullStream = DockerEngine.pull({ image: "docker.io/library/ubuntu:latest" });
+                        const pullStream = DockerEngine.pull({ image: "docker.io/library/alpine:latest" });
                         yield* MobyConvey.waitForProgressToComplete(pullStream);
 
                         const { Id: id } = yield* DockerEngine.run({
                             StopTimeout: I64.make(10n),
-                            Image: "docker.io/library/ubuntu:latest",
+                            Image: "docker.io/library/alpine:latest",
                             Cmd: ["sleep", "1m"],
                         });
 
@@ -86,7 +86,7 @@ describe.each(testMatrix)(
                     const containers = yield* MobyEndpoints.Containers;
                     const { Id: id } = yield* DockerEngine.run({
                         StopTimeout: I64.make(10n),
-                        Image: "docker.io/library/ubuntu:latest",
+                        Image: "docker.io/library/alpine:latest",
                         Cmd: ["sleep", "1s"],
                     });
 
