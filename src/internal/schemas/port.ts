214f2b63385505773a47e2052a0578333ca0d07a
Leo Conforti <leo@leoconforti.us>
2026-07-21 12:52:28 -0500

    migrate port schemas

--- src/internal/schemas/port.ts
+++ src/internal/schemas/port.ts
@@ -4,8 +4,9 @@
  * @since 1.0.0
  */
 
-import { Address, Port, PortWithMaybeProtocol } from "effect-schemas/Internet";
 import * as Schema from "effect/Schema";
+
+import { Address, Port, PortWithMaybeProtocol } from "effect-schemas/Internet";
 
 /**
  * A set of operating system ports.
@@ -14,14 +15,11 @@ import * as Schema from "effect/Schema";
  * @category Schemas
  * @see https://github.com/docker/go-connections/blob/5df8d2b30ca886f2d94740ce3c54abd58a5bb2c9/nat/nat.go#L23
  */
-export class PortSet extends Schema.Record({
-    key: PortWithMaybeProtocol.from,
-    value: Schema.Object, // TODO: What is this type?
-}).annotations({
+export const PortSet = Schema.Record(PortWithMaybeProtocol.from, Schema.ObjectKeyword).annotate({
     identifier: "PortSet",
     title: "A set of OS ports",
     description: "A set of operating system ports",
-}) {}
+});
 
 /**
  * A port binding between the exposed port (container) and the host.
@@ -30,14 +28,17 @@ export class PortSet extends Schema.Record({
  * @category Schemas
  * @see https://github.com/docker/go-connections/blob/5df8d2b30ca886f2d94740ce3c54abd58a5bb2c9/nat/nat.go#L11-L17
  */
-export class PortBinding extends Schema.Struct({
-    HostPort: Schema.compose(Schema.NumberFromString, Port),
-    HostIp: Schema.optional(Schema.Union(Schema.Literal(""), Address)),
-}).annotations({
-    identifier: "PortBinding",
-    title: "nat.PortBinding",
-    description: "A port binding between the exposed port (container) and the host",
-}) {}
+export class PortBinding extends Schema.Class<PortBinding>("PortBinding")(
+    {
+        HostPort: Schema.NumberFromString.pipe(Schema.decodeTo(Port)),
+        HostIp: Schema.optional(Schema.Union([Schema.Literal(""), Address])),
+    },
+    {
+        identifier: "PortBinding",
+        title: "nat.PortBinding",
+        description: "A port binding between the exposed port (container) and the host",
+    }
+) {}
 
 /**
  * Port mapping between the exposed port (container) and the host.
@@ -46,11 +47,8 @@ export class PortBinding extends Schema.Struct({
  * @category Schemas
  * @see https://github.com/docker/go-connections/blob/5df8d2b30ca886f2d94740ce3c54abd58a5bb2c9/nat/nat.go#L20
  */
-export class PortMap extends Schema.Record({
-    key: PortWithMaybeProtocol.from,
-    value: Schema.NullOr(Schema.Array(PortBinding)),
-}).annotations({
+export const PortMap = Schema.Record(PortWithMaybeProtocol.from, Schema.NullOr(Schema.Array(PortBinding))).annotate({
     identifier: "PortMap",
     title: "nat.PortMap",
     description: "Port mapping between the exposed port (container) and the host",
-}) {}
+});
