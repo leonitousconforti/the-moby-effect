d6cc7cbaeb3cab161f82256079b8df6edab375c5
Leo Conforti <leo@leoconforti.us>
2026-07-21 09:35:44 -0500

    save

--- src/internal/generated/NetworkCreateOptions.generated.ts
+++ src/internal/generated/NetworkCreateOptions.generated.ts
@@ -1,26 +1,26 @@
+import * as Effect from "effect/Effect";
 import * as Schema from "effect/Schema";
-import * as NetworkConfigReference from "./NetworkConfigReference.generated.js";
-import * as NetworkIPAM from "./NetworkIPAM.generated.js";
+import * as NetworkConfigReference from "./NetworkConfigReference.generated.ts";
+import * as NetworkIPAM from "./NetworkIPAM.generated.ts";
 
 export class NetworkCreateOptions extends Schema.Class<NetworkCreateOptions>("NetworkCreateOptions")(
     {
-        Driver: Schema.String.pipe(Schema.propertySignature).pipe(Schema.withConstructorDefault(() => "bridge")),
-        Scope: Schema.optionalWith(Schema.String, { nullable: true }),
-        EnableIPv4: Schema.optionalWith(Schema.Boolean, { nullable: true }),
-        EnableIPv6: Schema.optionalWith(Schema.Boolean, { nullable: true }),
-        IPAM: Schema.optionalWith(NetworkIPAM.NetworkIPAM, { nullable: true }),
-        Internal: Schema.optionalWith(Schema.Boolean, { nullable: true }),
-        Attachable: Schema.optionalWith(Schema.Boolean, { nullable: true }),
-        Ingress: Schema.optionalWith(Schema.Boolean, { nullable: true }),
-        ConfigOnly: Schema.Boolean.pipe(Schema.propertySignature).pipe(Schema.withConstructorDefault(() => false)),
-        ConfigFrom: Schema.optionalWith(NetworkConfigReference.NetworkConfigReference, { nullable: true }),
-        Options: Schema.optionalWith(Schema.Record({ key: Schema.String, value: Schema.String }), { nullable: true }),
-        Labels: Schema.optionalWith(Schema.Record({ key: Schema.String, value: Schema.String }), { nullable: true }),
+        Driver: Schema.String.pipe(Schema.withConstructorDefault(Effect.succeed("bridge"))),
+        Scope: Schema.optional(Schema.String),
+        EnableIPv4: Schema.optional(Schema.NullOr(Schema.Boolean)),
+        EnableIPv6: Schema.optional(Schema.NullOr(Schema.Boolean)),
+        IPAM: Schema.optional(Schema.NullOr(NetworkIPAM.NetworkIPAM)),
+        Internal: Schema.optional(Schema.Boolean),
+        Attachable: Schema.optional(Schema.Boolean),
+        Ingress: Schema.optional(Schema.Boolean),
+        ConfigOnly: Schema.Boolean.pipe(Schema.withConstructorDefault(Effect.succeed(false))),
+        ConfigFrom: Schema.optional(Schema.NullOr(NetworkConfigReference.NetworkConfigReference)),
+        Options: Schema.optional(Schema.NullOr(Schema.Record(Schema.String, Schema.String))),
+        Labels: Schema.optional(Schema.NullOr(Schema.Record(Schema.String, Schema.String))),
     },
     {
         identifier: "NetworkCreateOptions",
         title: "network.CreateOptions",
-        documentation:
-            "https://pkg.go.dev/github.com/docker/docker@v28.4.0+incompatible/api/types/network#CreateOptions",
+        documentation: "https://pkg.go.dev/github.com/docker/docker@v28.4.0+incompatible/api/types/network#CreateOptions",
     }
 ) {}
